2d52567a558f58135f3e3ffde286d4cd17365622
Keith Curbelo <keithcurbelo@yahoo.com>
2020-06-03 00:24:21 -0400

    create site footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import { Container } from "react-bootstrap";
+import IntroVideo from "./components/Intro";
+import AboutUs from "./components/AboutUs";
+import ContactForm from "./components/ContactForm";
+import Footer from "./components/Nav/Footer";
+import "./sass/App.scss";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <IntroVideo /> */}
+      <AboutUs />
+      <ContactForm />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Nav/Footer.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { Container, Col, Row } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <footer id="site-footer" className="footer text-center">
+      {/* <Row> */}
+      <div>Vis La Vie</div>
+      {/* </Row> */}
+    </footer>
+  );
+};
+
+export default Footer;
