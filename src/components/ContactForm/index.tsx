ad0497956496ba6a482eb9aeec5b2f84325ec850
Keith Curbelo <keithcurbelo@yahoo.com>
2020-06-03 00:28:32 -0400

    Put together simple contact form , no logic

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -1,10 +1,34 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Form, Col, Button } from "react-bootstrap";
+import Input from "./input";
 
 const ContactForm = () => {
+  const FullNameInput = () => (
+    <Input id="name" placeholder="Full name" name="name" />
+  );
+  const EmailInput = () => (
+    <Input id="email" placeholder="Email" name="email" />
+  );
+  const SubmitBtn = () => (
+    <Form.Row>
+      <Col xs={1} sm={4} />
+      <Col className="text-right">
+        <Button variant="dark" type="submit">
+          Submit
+        </Button>
+      </Col>
+      <Col xs={1} sm={4} />
+    </Form.Row>
+  );
+
   return (
-    <Container>
-      <h1>Hello from the contact form!!!</h1>
+    <Container className="py-3">
+      <h2 className="text-center py-3">Contact Us</h2>
+      <Form>
+        <FullNameInput />
+        <EmailInput />
+        <SubmitBtn />
+      </Form>
     </Container>
   );
 };
