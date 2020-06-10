a3c92508e55bf41f928048235bd3f842ec9071a5
Keith Curbelo <keithcurbelo@yahoo.com>
2020-06-09 23:26:33 -0400

    refactor intro video component

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -1,24 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 // import { Container, Col } from "react-bootstrap";
 import Video from "../../assets/videos/promo1.mp4";
 
-const IntroPanel = () => {
-  const [videoURL, setVideoURL] = useState(
-    "/src/assets/videos/islandsvideo.mp4"
-  );
-
-  return (
-    <video
-      id="background-video"
-      className="min-height-50 w-100"
-      loop
-      autoPlay
-      muted
-    >
-      <source src={Video} type="video/mp4" />
-      Your browser does not support the video tag.
-    </video>
-  );
-};
+const IntroPanel: React.FC = () => (
+  <video
+    id="background-video"
+    className="min-height-50 w-100"
+    loop
+    autoPlay
+    muted
+  >
+    <source src={Video} type="video/mp4" />
+    Your browser does not support the video tag.
+  </video>
+);
 
 export default IntroPanel;
