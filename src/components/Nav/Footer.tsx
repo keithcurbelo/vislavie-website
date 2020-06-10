760fb83d33012ab872be508825d4aa3a94276d69
Keith Curbelo <keithcurbelo@yahoo.com>
2020-06-09 21:13:00 -0400

    create site footer component with scss

--- src/components/Nav/Footer.tsx
+++ src/components/Nav/Footer.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import { Container, Col, Row } from "react-bootstrap";
+// import { Container, Col, Row } from "react-bootstrap";
+import { getCurrentYear } from "../../utils/appHelpers";
 
 const Footer = () => {
+  const [currentYear, setCurrentYear] = useState(getCurrentYear());
   return (
-    <footer id="site-footer" className="footer text-center">
+    <footer id="main-footer" className="footer text-center">
       {/* <Row> */}
       <div>Vis La Vie</div>
       {/* </Row> */}
