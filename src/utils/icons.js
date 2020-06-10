52529a784556f1f6bef972793231dfe2e2f6c970
Keith Curbelo <keithcurbelo@yahoo.com>
2020-06-09 21:08:52 -0400

    create an icon component

---
+++ src/utils/icons.js
@@ -0,0 +1,43 @@
+// import { library } from "@fortawesome/fontawesome-svg-core";
+// import { fab } from "@fortawesome/free-brands-svg-icons";
+// import { far } from "@fortawesome/free-regular-svg-icons";
+// import {
+//   faEdit,
+//   faHome,
+//   faSignOutAlt,
+//   faTools,
+//   faHourglassHalf,
+//   faBusinessTime,
+//   faClock,
+//   faCalendarAlt,
+//   faCaretUp,
+//   faCaretDown,
+//   faPlus,
+//   faSearch,
+//   faInfo,
+//   faHeadset,
+//   faTimes,
+//   faQuestionCircle,
+//   faUsers,
+// } from "@fortawesome/free-solid-svg-icons";
+
+// library.add(
+//   far,
+//   faPlus,
+//   faEdit,
+//   faHome,
+//   faSignOutAlt,
+//   faHourglassHalf,
+//   faBusinessTime,
+//   faClock,
+//   faCalendarAlt,
+//   faTools,
+//   faCaretUp,
+//   faCaretDown,
+//   faSearch,
+//   faInfo,
+//   faHeadset,
+//   faTimes,
+//   faQuestionCircle,
+//   faUsers
+// );
