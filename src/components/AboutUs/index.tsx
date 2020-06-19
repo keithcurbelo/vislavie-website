a7a3515ac78b33f81fe1e67ee29eac2df61742c4
Keith Curbelo <keithcurbelo@yahoo.com>
2020-06-18 23:13:37 -0400

    make AboutUs component responsive

--- src/components/AboutUs/index.tsx
+++ src/components/AboutUs/index.tsx
@@ -1,30 +1,28 @@
 import React from "react";
 import { Container, Col, Row } from "react-bootstrap";
-// import { useWindowSize } from "../../utils/components";
+import { useWindowSize } from "../../utils/components";
 import ImageTiles from "./ImageTiles";
+import ImageCarousel from "./ImageCarousel";
+import CompanyDescription from "./CompanyDescription";
 
 const AboutUs = () => {
-  // const [width, height] = useWindowSize();
-  return (
-    <Container fluid className="min-vh-100">
-      {/* <Row className="w-100">
-        <Col className="text-center text-uppercase">
-          <h2> About Us</h2>
-        </Col>
-      </Row> */}
-      <Row className="w-100">
-        <Col className="text-center text-uppercase">
-          <h4 className="font-weight-bold">
-            Everything you need for the ultimate luxury experience
-          </h4>
-          <h6 className="font-weight-light">
-            You deserve nothing other than best
-          </h6>
-        </Col>
-      </Row>
-      <ImageTiles />
-    </Container>
-  );
+  const [width] = useWindowSize();
+  // console.log("width", width);
+  if (width >= 768) {
+    return <ImageCarousel />;
+  } else {
+    return (
+      <Container fluid className="min-vh-100">
+        <Row className="w-100">
+          <Col className="text-center text-uppercase">
+            <h2> About Us</h2>
+          </Col>
+        </Row>
+        <CompanyDescription />
+        <ImageTiles />
+      </Container>
+    );
+  }
 };
 
 export default AboutUs;
