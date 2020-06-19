76c69ae29a252db3b28c83feaa5e47e69394b8f4
Keith Curbelo <keithcurbelo@yahoo.com>
2020-06-18 22:57:28 -0400

    update img carousel to use dynamic list

--- src/components/AboutUs/ImageCarousel.tsx
+++ src/components/AboutUs/ImageCarousel.tsx
@@ -1,39 +1,23 @@
 import React, { useLayoutEffect, useState } from "react";
-import { Container, Col, Row, Carousel } from "react-bootstrap";
+import { Carousel } from "react-bootstrap";
+import Images from "./images.json";
 // import { useWindowSize } from "../../utils/components";
 //images
-import Yacht from "../../assets/images/yatch.jpeg";
-import BlackLambo from "../../assets/images/black_lambo.jpg";
-import WhiteLambo from "../../assets/images/white_lambo.jpeg";
 
 const ImageCarousel: React.FC = () => (
-  <Row>
-    <Carousel className="w-100 py-3">
-      <Carousel.Item>
-        <img className="d-block w-100" src={Yacht} alt="First slide" />
+  // <Row className="vh-100">
+  <Carousel interval={3000} className="w-100">
+    {Images.list.map((el: any, i: number) => (
+      <Carousel.Item key={i}>
+        <img className="carousel-img" src={el.src} alt="First slide" />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3>{el.title}</h3>
+          <p>{el.description}</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
-        <img className="d-block w-100" src={BlackLambo} alt="Third slide" />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img className="d-block w-100" src={WhiteLambo} alt="Third slide" />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  </Row>
+    ))}
+  </Carousel>
+  // </Row>
 );
 
 export default ImageCarousel;
