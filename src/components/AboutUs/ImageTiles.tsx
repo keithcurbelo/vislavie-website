0de5dcb11e11afc01258d3d860476f5520c4b029
Keith Curbelo <keithcurbelo@yahoo.com>
2020-06-18 23:12:25 -0400

    refactor image tile to use image.json

--- src/components/AboutUs/ImageTiles.tsx
+++ src/components/AboutUs/ImageTiles.tsx
@@ -1,35 +1,20 @@
 import React from "react";
 import { Col, Row, Image } from "react-bootstrap";
 // import { useWindowSize } from "../../utils/components";
-//images
-// import YachtTable from "../../assets/images/boat-table.jpg";
-import Yacht from "../../assets/images/miami-yacht.jpg";
-// import BlackLambo from "../../assets/images/black_lambo.jpg";
-import WhiteLambo from "../../assets/images/white_lambo.jpeg";
-import Villa from "../../assets/images/villa2.jpg";
+import Images from "./images.json";
 
 const ImageTiles: React.FC = () => (
   <Row className="text-center">
-    <Col xs={12} className="p-md-5 p-3">
-      <div className="w-100">
-        <Image className="img-tile" src={Yacht} rounded fluid />
-      </div>
-      <div className="w-50 my-3 mx-auto border-top border-dark ">
-        <h4>Yacht Rentals</h4>
-      </div>
-    </Col>
-    <Col xs={12} className="p-md-5 p-3">
-      <Image className="img-tile" src={WhiteLambo} rounded fluid />
-      <div className="w-50 my-3 mx-auto border-top border-dark text-center">
-        <h4>Exotic Cars</h4>
-      </div>
-    </Col>
-    <Col xs={12} className="p-md-5 p-3">
-      <Image className="img-tile" src={Villa} rounded fluid />
-      <div className="w-50 my-3 mx-auto border-top border-dark text-center">
-        <h4>Luxury Villas</h4>
-      </div>
-    </Col>
+    {Images.list.map((el: any, i: number) => (
+      <Col key={i} xs={12} className="p-md-5 p-3">
+        <div className="w-100">
+          <Image className="img-tile" src={el.src} rounded fluid />
+        </div>
+        <div className="w-50 my-3 mx-auto border-top border-dark ">
+          <h4>{el.title}</h4>
+        </div>
+      </Col>
+    ))}
   </Row>
 );
 
