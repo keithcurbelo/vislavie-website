656ad8a720de05ebd441f259c53d6edffda3b4a3
Keith Curbelo <keithcurbelo@yahoo.com>
2020-06-22 20:35:39 -0400

    add apphelper to format data

--- src/utils/appHelpers.tsx
+++ src/utils/appHelpers.tsx
@@ -2,3 +2,9 @@ export const getCurrentYear = () => {
   let date = new Date();
   return date.getFullYear();
 };
+
+export const formatDataEntry = (arr: Array<any>) => ({
+  name: arr[0].value,
+  email: arr[1].value,
+  services: arr[2].value,
+});
