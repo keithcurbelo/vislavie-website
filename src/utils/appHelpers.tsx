624818ee786f72faef913b37b733730f22dc3b93
Keith Curbelo <keithcurbelo@yahoo.com>
2020-06-22 21:33:27 -0400

    add apphelpers for phone numbers and emails

--- src/utils/appHelpers.tsx
+++ src/utils/appHelpers.tsx
@@ -8,3 +8,27 @@ export const formatDataEntry = (arr: Array<any>) => ({
   email: arr[1].value,
   services: arr[2].value,
 });
+
+export const isValidEmail = (email: string) => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+};
+
+export const isValidPhoneNumber = (phone_number: string) => {
+  const regex = /^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/;
+  return regex.test(phone_number);
+};
+
+export const formatPhoneNumber = (
+  phone_number: string,
+  rawVal: boolean = false
+) => {
+  phone_number = phone_number.toString();
+  const regex = /^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/;
+  const formatArr = phone_number.match(regex) || [];
+  if (regex.test(phone_number)) {
+    if (rawVal) return phone_number.replace(/[()-\s]/g, "");
+    return `(${formatArr[1]}) ${formatArr[2]}-${formatArr[3]}`;
+  }
+
+  return phone_number.replace(/[()-\s]/g, "");
+};
