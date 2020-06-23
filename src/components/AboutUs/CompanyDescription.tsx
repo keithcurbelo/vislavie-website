53861ea5b0b0ca6fe5ddce0f2d2b6eaa77c0b678
Keith Curbelo <keithcurbelo@yahoo.com>
2020-06-22 23:25:21 -0400

    fix spacing for company description

--- src/components/AboutUs/CompanyDescription.tsx
+++ src/components/AboutUs/CompanyDescription.tsx
@@ -3,17 +3,21 @@ import { Col, Row } from "react-bootstrap";
 
 const CompanyDescription: React.FC = () => {
   return (
-    <Row className="w-100">
+    <Row className="">
       <Col className="text-center">
-        <p>
+        <p className="p-3 font-sm">
           Discover the unparalleled experience that is Vis La Vie. With us, your
           dreams become a reality. Prepare yourself for an unforgettable
-          adventure that eill be completely tailored to you. Experience white
+          adventure that eill be completely tailored to you.
+          {/* Experience white
           glove service paired with exceptional scenic water views. Explore
           secluded sandbars, 5 star restaurants, local marine life and so much
-          more.
+          more. */}
         </p>
-        <p>Welcome to South Florida. The boating capital of the world.</p>
+        <p>
+          Welcome to South Florida. <br />
+          The boating capital of the world.
+        </p>
       </Col>
       {/* <Row className="w-100">
         <Col className="text-center text-uppercase">
