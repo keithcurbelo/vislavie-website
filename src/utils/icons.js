19f55036d1b04d19d438314ad83c635fdac5ab79
Keith Curbelo <keithcurbelo@yahoo.com>
2020-06-22 21:31:57 -0400

    update icon import

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,10 +1,6 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  fab,
-  faFacebook,
-  faInstagram,
-} from "@fortawesome/free-brands-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 // import { far } from "@fortawesome/free-regular-svg-icons";
 // import { fa } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faFacebook, faInstagram);
+library.add(fab);
