cf49177e690d5364fd9401aa4a9a9e8d86d1f092
Keith Curbelo <keithcurbelo@yahoo.com>
2020-06-22 20:32:45 -0400

    refactor intro media

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 // import { Container, Col } from "react-bootstrap";
 import PromoVideo from "./video";
+import PromoImage from "./logo";
+import { useWindowSize } from "../../utils/components";
 
-const IntroPanel: React.FC = () => <PromoVideo />;
+const IntroPanel: React.FC = () => {
+  const [width] = useWindowSize();
+
+  if (width > 400) {
+    return <PromoVideo />;
+  } else {
+    return <PromoImage />;
+  }
+};
 
 export default IntroPanel;
--- src/components/Intro/video.tsx
+++ src/components/Intro/video.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 
 const PromoVideo: React.FC = () => {
+  const baseURL = process.env.CDN_URL || "https://media.vislaviemiami.com";
   return (
     <video id="promo-video" loop autoPlay muted>
-      <source
-        src="https://media.vislaviemiami.com/videos/aerial-vid.mov"
-        type="video/mp4"
-      />
+      <source src={`${baseURL}/videos/aerial-vid.mov`} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   );
