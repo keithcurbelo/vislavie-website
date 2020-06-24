8b0075bd9680af344b9e7fc98ffcf29f93ca4c10
Keith Curbelo <keithcurbelo@yahoo.com>
2020-06-23 23:16:23 -0400

    add form  back to page

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => {
       <div id="content-wrap">
         <IntroMedia />
         <AboutUs />
-        {/* <ContactForm /> */}
+        <ContactForm />
         <Footer />
       </div>
     </div>
