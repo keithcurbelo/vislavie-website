8b2195ef047b0ff2def5cdfbbf2613abd37b3e52
Keith Curbelo <keithcurbelo@yahoo.com>
2020-06-24 21:13:59 -0400

    be explicit for mute prop on intro video

--- src/components/Intro/video.tsx
+++ src/components/Intro/video.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const PromoVideo: React.FC = () => {
   const baseURL = process.env.CDN_URL || "https://media.vislaviemiami.com";
   return (
-    <video id="promo-video" loop autoPlay>
+    <video id="promo-video" loop autoPlay muted={false}>
       <source src={`${baseURL}/videos/aerial-vid.mov`} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
