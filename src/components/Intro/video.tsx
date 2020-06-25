f73b07870e6d3cabfbc521d693f70d7d2cc95c7b
Keith Curbelo <keithcurbelo@yahoo.com>
2020-06-24 22:49:38 -0400

    update video url

--- src/components/Intro/video.tsx
+++ src/components/Intro/video.tsx
@@ -4,7 +4,7 @@ const PromoVideo: React.FC = () => {
   const baseURL = process.env.CDN_URL || "https://media.vislaviemiami.com";
   return (
     <video id="promo-video" loop autoPlay muted={false}>
-      <source src={`${baseURL}/videos/aerial-vid.mov`} type="video/mp4" />
+      <source src={`${baseURL}/videos/aerial-vid.mp4`} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   );
